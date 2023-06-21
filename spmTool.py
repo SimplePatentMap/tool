32b0ac578763a9d5186b60b9f48c7d99fff11434
SimplePatentMap <133883067+SimplePatentMap@users.noreply.github.com>
2023-06-21 15:41:16 +0900

    Add files via upload
    spmToolの表示部分を修正

--- spmTool.py
+++ spmTool.py
@@ -10,7 +10,7 @@ with st.sidebar:
     st.markdown("[J-PlatPat](https://www.j-platpat.inpit.go.jp/)")
 #    st.markdown("[使い方](https://www.j-platpat.inpit.go.jp/)")
 
-uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+uploaded_files = st.file_uploader("CSVファイルを選択して下さい", accept_multiple_files=True)
 df = pd.DataFrame()
 for uploaded_file in uploaded_files:
     df = pd.concat([df, pd.read_csv(uploaded_file)])
@@ -25,9 +25,9 @@ if len(df) > 1:
     #Downloadボタンの追加
     formatted_df.to_excel(buf := BytesIO(), index=False)
     st.download_button(
-        "Download",
+        "ダウンロード",
         buf.getvalue(),
-        "sample.xlsx",
+        "simplepatentmap.xlsx",
     )
 
 
